e956a06834e4d40f4ada98df9e8a77746141ca1f
Theo Goulart <theo_goulart@hotmail.com>
2017-03-30 20:12:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+
+var app = express()
+
+app.get('/', function(req, res){
+    
+    var result = {}
+    
+    result.url = ""
+    result.snippet = ""
+    result.thumbnail = ""
+    result.context = ""
+    
+    res.status(200).send(result)
+})
+
+app.get('/latest/', function(req, res){
+    
+    var result = {}
+    
+    result.term = ""
+    result.when = ""
+    
+    res.status(200).send(result)
+})
