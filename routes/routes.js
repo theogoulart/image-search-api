42e86bccc351af45120ab36ed6cbcf0490a2df42
Theo Goulart <theo_goulart@hotmail.com>
2017-03-31 23:22:18 +0000

    add request to bing image api and show results

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,35 @@
-var router = require('express').Router()
+var router  = require('express').Router()
+var request = require('request')
+
+var options = {
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=cats&count=10&offset=0',
+    headers: {
+        'Ocp-Apim-Subscription-key': process.env.IMAGEAPI_KEY
+    },
+    json: true
+}
 
 router.get('/', function(req, res){
     
-    var result = {}
-    
-    result.url = ""
-    result.snippet = ""
-    result.thumbnail = ""
-    result.context = ""
-    
-    req.database.close()
-    res.status(200).send(result)
+    request.get(options,function(err, response, body){
+        if (err) throw err
+        
+        var result = []
+        
+        body.value.forEach(function(val){
+            
+            var element = {}
+            
+            element.url = val.contentUrl
+            element.snippet = val.name
+            element.thumbnail = val.thumbnailUrl
+            element.context = val.hostPageDisplayUrl
+            
+            result.push(element)
+        })
+        res.status(200).send(result)
+        req.database.close()
+    })
 })
 
 router.get('/latest/', function(req, res){
