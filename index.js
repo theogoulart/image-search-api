449f71be8e326b22476b42ef26db1fae368b29bc
Theo Goulart <theo_goulart@hotmail.com>
2017-04-01 02:28:19 +0000

    add Procfile and mLab URI

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var mongo = require('mongodb').MongoClient
 var route = require('./routes/routes')
 
-var dbURL = 'mongodb://localhost:27017/local'
+var dbURL = process.env.MONGOLAB_URI
 var app = express()
 
 app.use(function(req, res, next){
@@ -17,7 +17,7 @@ app.use(route)
 
 app.use(function(req, res){
     req.database.close()
-    res.status(404).send("Cannot find path, use either '/' or '/latest/'")
+    res.status(404).send("Cannot find path, use either '/imagesearch/<term>?offset=<num>' or '/latest/'")
 })
 
 .listen(process.env.PORT || 8080)
